Spickelbing/corrodoro

commit e8eb0922d1e665aab47b19a919348cd107cd5e9f

feat: correctly display network status for clients
this includes a little refactoring of the protocol

src/app.rs
@@ -1,5 +1,5 @@
-use crate::net::{Client, ClientMessage, NetworkError, Server, ServerMessage};
-use crate::pomodoro;
+use crate::net::{Client, ClientMessage, Event, NetworkError, Server, ServerMessage, TimerVisuals};
+use crate::pomodoro::{Activity, State};
 use crate::tui::{Tui, TuiError};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
@@ -12,13 +12,13 @@ use tokio::select;
 use tokio::time::{interval, Interval};
 
 pub struct App {
-    pomodoro_state: pomodoro::State,
+    pomodoro_state: State,
     tui: Tui,
     server: Option<Server>,
 }
 
 impl App {
-    pub fn new(pomodoro_state: pomodoro::State) -> Result<Self, UnrecoverableError> {
+    pub fn new(pomodoro_state: State) -> Result<Self, UnrecoverableError> {
         let tui = Tui::new()?;
 
         Ok(Self {
@@ -50,37 +50,39 @@ impl App {
         Ok(())
     }
 
+    async fn broadcast_visuals(&mut self, visuals: TimerVisuals) {
+        if let Some(server) = &mut self.server {
+            let maybe_bytes = ServerMessage::Display(visuals).as_bytes();
+            if let Ok(msg) = maybe_bytes {
+                // ignore transmission errors for now
+                let _ = server.broadcast_frame(msg).await;
+            } else {
+                // this should never happen
+            }
+        }
+    }
+
+    async fn broadcast_notify(&mut self, next_activity: Activity) {
+        if let Some(server) = &mut self.server {
+            let maybe_bytes = ServerMessage::Notify(next_activity).as_bytes();
+            if let Ok(msg) = maybe_bytes {
+                // ignore transmission errors for now
+                let _ = server.broadcast_frame(msg).await;
+            } else {
+                // this should never happen
+            }
+        }
+    }
+
     async fn run_inner(&mut self) -> Result<(), UnrecoverableError> {
         let mut pomodoro_clock = interval(Duration::from_millis(100));
         let mut pomodoro_start_time = Instant::now();
 
-        let mut notify_end_of_activity = false;
         loop {
-            let ui_data = Display::from(&*self);
-            let ui_data = Display {
-                notify_end_of_activity,
-                ..ui_data
-            };
-
-            self.tui.render(&ui_data)?;
-
-            if let Some(server) = &mut self.server {
-                let visuals = Display {
-                    mode_info: AppModeInfo::Client {
-                        connected_to: server.local_addr,
-                    },
-                    ..ui_data
-                };
-
-                let maybe_msg = ServerMessage::new(visuals);
-                if let Ok(msg) = maybe_msg {
-                    // ignore transmission errors for now
-                    let _ = server.broadcast_frame(msg.into()).await;
-                } else {
-                    // this should never happen
-                }
-            }
-            notify_end_of_activity = false;
+            let visuals = TimerVisuals::from(&*self);
+            let network_status = NetworkStatus::from(&*self);
+            self.tui.render(&visuals, &network_status)?;
+            self.broadcast_visuals(visuals).await;
 
             select! {
                 _ = pomodoro_clock.tick() => {
@@ -92,29 +94,29 @@ impl App {
 
                         let activity_after = self.pomodoro_state.current_activity();
                         if activity_before != activity_after {
-                            notify_end_of_activity = true;
+                            self.broadcast_notify(activity_after).await;
                         }
                     }
                 }
-                event = self.tui.read_event() => {
-                    let event = event?;
+                tui_event = self.tui.read_event() => {
+                    let event = tui_event?;
                     if *self.handle_event(&event, &mut pomodoro_clock, &mut pomodoro_start_time) {
                         break;
                     }
                 }
-                event = async {
+                client_msg = async {
                     match &mut self.server {
                         Some(server) => {
                             match server.recv_frame().await {
-                                Ok(frame) => ClientMessage::deserialize(&frame),
+                                Ok(frame) => ClientMessage::from_bytes(frame),
                                 Err(err) => Err(err),
                             }
                         }
                         _ => ForeverPending.await.forever(),
                     }
                 } => {
-                    match event {
-                        Ok(event) => {
+                    match client_msg {
+                        Ok(ClientMessage::Event(event)) => {
                             if *self.handle_event(&event, &mut pomodoro_clock, &mut pomodoro_start_time) {
                                 break;
                             }
@@ -194,6 +196,8 @@ impl ClientApp {
     }
 
     async fn run_inner(&mut self) -> Result<(), UnrecoverableError> {
+        let network_status = NetworkStatus::from(&*self);
+
         loop {
             // TODO: what about resize events?
             select! {
@@ -204,13 +208,16 @@ impl ClientApp {
                         break;
                     }
 
-                    let msg = ClientMessage::new(event)?;
-                    self.client.send_frame(msg.into()).await?;
+                    let bytes = ClientMessage::Event(event).as_bytes()?;
+                    self.client.send_frame(bytes).await?;
                 }
                 msg = self.client.recv_frame() => {
                     let msg = msg?;
-                    let visuals = ServerMessage::deserialize(&msg)?;
-                    self.tui.render(&visuals)?;
+                    let msg = ServerMessage::from_bytes(msg)?;
+                    match msg {
+                        ServerMessage::Display(visuals) => self.tui.render(&visuals, &network_status)?,
+                        ServerMessage::Notify(activity) => self.tui.show_notification(activity),
+                    }
                 }
             }
         }
@@ -229,51 +236,20 @@ impl Deref for AppShouldQuit {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Event {
-    Quit,
-    ToggleTimer,
-    ResetTimer,
-    SkipActivity,
-    ExtendActivity(Duration),
-    ReduceActivity(Duration),
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Display {
-    pub time_remaining: pomodoro::SessionDuration,
-    pub timer_is_paused: bool,
-    pub activity: pomodoro::Activity,
-    pub progress_percentage: f64,
-    pub completed_focus_sessions: u32,
-    pub notify_end_of_activity: bool,
-    pub mode_info: AppModeInfo,
-}
-
-impl From<&App> for Display {
+impl From<&App> for TimerVisuals {
     fn from(app: &App) -> Self {
-        let mode_info = match &app.server {
-            Some(server) => AppModeInfo::Server {
-                connected_clients: server.clients(),
-                listening_on: server.local_addr,
-            },
-            None => AppModeInfo::Offline,
-        };
-
-        Display {
+        TimerVisuals {
             time_remaining: app.pomodoro_state.time_remaining(),
             timer_is_paused: !app.pomodoro_state.timer_is_active(),
             activity: app.pomodoro_state.current_activity(),
             progress_percentage: app.pomodoro_state.progress_percentage(),
             completed_focus_sessions: app.pomodoro_state.completed_focus_sessions(),
-            notify_end_of_activity: false,
-            mode_info,
         }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum AppModeInfo {
+pub enum NetworkStatus {
     Offline,
     Server {
         connected_clients: Vec<SocketAddr>,
@@ -282,6 +258,26 @@ pub enum AppModeInfo {
     Client {
         connected_to: SocketAddr,
     },
+}
+
+impl From<&App> for NetworkStatus {
+    fn from(app: &App) -> Self {
+        match &app.server {
+            Some(server) => NetworkStatus::Server {
+                connected_clients: server.clients(),
+                listening_on: server.local_addr,
+            },
+            None => NetworkStatus::Offline,
+        }
+    }
+}
+
+impl From<&ClientApp> for NetworkStatus {
+    fn from(app: &ClientApp) -> Self {
+        NetworkStatus::Client {
+            connected_to: app.client.remote_addr,
+        }
+    }
 }
 
 /// Represents errors the app has no control over.

src/net.rs
@@ -9,7 +9,7 @@ mod protocol;
 mod server;
 
 pub use client::Client;
-pub use protocol::{ClientMessage, ServerMessage};
+pub use protocol::{ClientMessage, Event, ServerMessage, TimerVisuals};
 pub use server::Server;
 
 type FramedStream = Framed<TcpStream, LengthDelimitedCodec>;

src/net/protocol.rs
@@ -1,52 +1,91 @@
-use crate::app::{Display, Event};
 use crate::net::NetworkError;
+use crate::pomodoro::{Activity, SessionDuration};
 use bytes::Bytes;
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
 #[derive(Serialize, Deserialize)]
 pub enum ClientMessage {
-    Event(Bytes),
+    Event(Event),
 }
 
 #[derive(Serialize, Deserialize)]
 pub enum ServerMessage {
-    Display(Bytes),
+    Display(TimerVisuals),
+    Notify(Activity),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Event {
+    Quit,
+    ToggleTimer,
+    ResetTimer,
+    SkipActivity,
+    ExtendActivity(Duration),
+    ReduceActivity(Duration),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TimerVisuals {
+    pub time_remaining: SessionDuration,
+    pub timer_is_paused: bool,
+    pub activity: Activity,
+    pub progress_percentage: f64,
+    pub completed_focus_sessions: u32,
 }
 
 impl ClientMessage {
-    pub fn new(event: Event) -> Result<Self, NetworkError> {
-        let serialized = bincode::serialize(&event).map_err(NetworkError::Serialize)?;
-        Ok(Self::Event(serialized.into()))
+    pub fn as_bytes(&self) -> Result<Bytes, NetworkError> {
+        self.try_into()
     }
 
-    pub fn deserialize(bytes: &[u8]) -> Result<Event, NetworkError> {
-        bincode::deserialize(bytes).map_err(NetworkError::Deserialize)
+    pub fn from_bytes(bytes: Bytes) -> Result<Self, NetworkError> {
+        bytes.try_into()
     }
 }
 
-impl From<ClientMessage> for Bytes {
-    fn from(message: ClientMessage) -> Self {
-        match message {
-            ClientMessage::Event(bytes) => bytes,
-        }
+impl TryFrom<Bytes> for ClientMessage {
+    type Error = NetworkError;
+
+    fn try_from(bytes: Bytes) -> Result<Self, Self::Error> {
+        bincode::deserialize(&bytes).map_err(NetworkError::Deserialize)
+    }
+}
+
+impl TryFrom<&ClientMessage> for Bytes {
+    type Error = NetworkError;
+
+    fn try_from(msg: &ClientMessage) -> Result<Self, Self::Error> {
+        bincode::serialize(&msg)
+            .map_err(NetworkError::Serialize)
+            .map(|v| v.into())
     }
 }
 
 impl ServerMessage {
-    pub fn new(visuals: Display) -> Result<Self, NetworkError> {
-        let serialized = bincode::serialize(&visuals).map_err(NetworkError::Serialize)?;
-        Ok(Self::Display(serialized.into()))
+    pub fn as_bytes(&self) -> Result<Bytes, NetworkError> {
+        self.try_into()
     }
 
-    pub fn deserialize(bytes: &[u8]) -> Result<Display, NetworkError> {
-        bincode::deserialize(bytes).map_err(NetworkError::Deserialize)
+    pub fn from_bytes(bytes: Bytes) -> Result<Self, NetworkError> {
+        bytes.try_into()
     }
 }
 
-impl From<ServerMessage> for Bytes {
-    fn from(message: ServerMessage) -> Self {
-        match message {
-            ServerMessage::Display(bytes) => bytes,
-        }
+impl TryFrom<Bytes> for ServerMessage {
+    type Error = NetworkError;
+
+    fn try_from(bytes: Bytes) -> Result<Self, Self::Error> {
+        bincode::deserialize(&bytes).map_err(NetworkError::Deserialize)
+    }
+}
+
+impl TryFrom<&ServerMessage> for Bytes {
+    type Error = NetworkError;
+
+    fn try_from(msg: &ServerMessage) -> Result<Self, Self::Error> {
+        bincode::serialize(&msg)
+            .map_err(NetworkError::Serialize)
+            .map(|v| v.into())
     }
 }

src/tui.rs
@@ -1,5 +1,7 @@
-use crate::app::{Display, Event};
+use crate::app::NetworkStatus;
+use crate::net::{Event, TimerVisuals};
 use crate::notification;
+use crate::pomodoro::Activity;
 use crossterm::event::{
     Event as CrosstermEvent, EventStream, KeyCode, KeyEventKind, KeyModifiers, MouseEventKind,
 };
@@ -71,19 +73,29 @@ impl Tui {
         Ok(())
     }
 
-    pub fn render(&mut self, render_data: &Display) -> Result<(), TuiError> {
+    pub fn render(
+        &mut self,
+        timer_visuals: &TimerVisuals,
+        network_status: &NetworkStatus,
+    ) -> Result<(), TuiError> {
         self.terminal
             .draw(|f| {
-                render::render_ui(f, render_data, self.show_settings, self.show_timer);
+                render::render_ui(
+                    f,
+                    timer_visuals,
+                    network_status,
+                    self.show_settings,
+                    self.show_timer,
+                );
             })
             .map_err(TuiError::Rendering)?;
 
-        if render_data.notify_end_of_activity {
-            // ignore errors for now, perhaps add a log message in the tui in the future
-            let _ = notification::show_desktop_notification("", &render_data.activity.to_string());
-        }
-
         Ok(())
+    }
+
+    pub fn show_notification(&self, new_activity: Activity) {
+        // ignore errors for now, perhaps add a log message in the tui in the future
+        let _ = notification::show_desktop_notification("", &new_activity.to_string());
     }
 
     pub async fn read_event(&mut self) -> Result<Event, TuiError> {
@@ -151,6 +163,7 @@ pub struct EventConversionUndefinedError;
 impl TryFrom<CrosstermEvent> for Event {
     type Error = EventConversionUndefinedError;
 
+    // TODO: accept uppercase chars too
     fn try_from(event: CrosstermEvent) -> Result<Self, Self::Error> {
         match event {
             CrosstermEvent::Key(key_event)

src/tui/render.rs
@@ -1,5 +1,5 @@
-use crate::app::AppModeInfo;
-use crate::app::Display;
+use crate::app::NetworkStatus;
+use crate::net::TimerVisuals;
 use crate::tui::animation;
 use crate::tui::widgets::{BlockWithLegend, Settings};
 use std::io;
@@ -56,7 +56,8 @@ fn define_block<'a>(title: &'a str, legend: Vec<&'a str>) -> BlockWithLegend<'a>
 
 pub fn render_ui(
     frame: &mut Frame<CrosstermBackend<io::Stdout>>,
-    render_data: &Display,
+    timer_visuals: &TimerVisuals,
+    network_status: &NetworkStatus,
     show_settings: bool,
     show_timer: bool,
 ) {
@@ -127,15 +128,15 @@ pub fn render_ui(
     };
 
     let widget_clock_animation = {
-        let partial_clock = animation::clock(1.0 - render_data.progress_percentage);
+        let partial_clock = animation::clock(1.0 - timer_visuals.progress_percentage);
 
         widgets::Paragraph::new(partial_clock).alignment(Alignment::Left)
     };
 
     let clock_text = {
-        let is_fourth_session = render_data.completed_focus_sessions % 4 == 0;
-        let n_highlighted_indicators: usize = render_data.completed_focus_sessions as usize % 4
-            + match (render_data.activity.is_focus(), is_fourth_session) {
+        let is_fourth_session = timer_visuals.completed_focus_sessions % 4 == 0;
+        let n_highlighted_indicators: usize = timer_visuals.completed_focus_sessions as usize % 4
+            + match (timer_visuals.activity.is_focus(), is_fourth_session) {
                 (true, _) => 1,
                 (false, true) => 4,
                 _ => 0,
@@ -144,9 +145,9 @@ pub fn render_ui(
         format!(
             "{}\n{}\n{} {}",
             animation::session_counter(n_highlighted_indicators, 4),
-            render_data.time_remaining,
-            render_data.activity,
-            if render_data.timer_is_paused {
+            timer_visuals.time_remaining,
+            timer_visuals.activity,
+            if timer_visuals.timer_is_paused {
                 "⏵"
             } else {
                 "⏸"
@@ -176,9 +177,9 @@ pub fn render_ui(
     let widget_clock_text = widgets::Paragraph::new(clock_text).alignment(Alignment::Center);
 
     let network_info_text = {
-        match &render_data.mode_info {
-            AppModeInfo::Offline => "offline".to_string(),
-            AppModeInfo::Server {
+        match &network_status {
+            NetworkStatus::Offline => "offline".to_string(),
+            NetworkStatus::Server {
                 connected_clients,
                 listening_on,
             } => {
@@ -193,7 +194,7 @@ pub fn render_ui(
                     listening_on.port()
                 )
             }
-            AppModeInfo::Client { connected_to } => {
+            NetworkStatus::Client { connected_to } => {
                 format!("connected to {connected_to}")
             }
         }

commit 8e3d179e6ed918fd233717f269b178c519bbe218

fix: show notification on server
was broken after e8eb0922

src/app.rs
@@ -95,6 +95,7 @@ impl App {
                         let activity_after = self.pomodoro_state.current_activity();
                         if activity_before != activity_after {
                             self.broadcast_notify(activity_after).await;
+                            self.tui.show_notification(activity_after);
                         }
                     }
                 }